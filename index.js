2681614e77769ceb231b8dd9019efcfc15f9d60a
Jayachandran <159675826+jai3747@users.noreply.github.com>
2025-02-24 23:11:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:3000",
+  origin: process.env.FRONTEND_URL || "http://34.47.205.16:3000",
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
